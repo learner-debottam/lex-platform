26bdee05c5190e807a933c0f1241bce3857b8aaf
Debottam Barman Roy <learner.debottam@gmail.com>
2026-02-11 23:00:48 +0000

    feat: store the state file in s3

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "company-terraform-state"
-#     key            = "lex/dev/terraform.tfstate"
-#     region         = "eu-west-2"
-#     dynamodb_table = "terraform-locks"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "lex-platform-tfstate"
+    key            = "lex/dev/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "lex-platform-tfstate-lock"
+    encrypt        = true
+  }
+}
