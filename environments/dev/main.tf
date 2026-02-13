405514408fef63766e7d2292b382931e30c7d7b0
Debottam Barman Roy <learner.debottam@gmail.com>
2026-02-13 19:46:53 +0000

    refactor: enabled pizza bot

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -22,10 +22,10 @@
 locals {
   bot_config = jsondecode(
     //file("${path.module}/../../bots/customer-support/bot.json")
-    file("${path.module}/../../bots/examples/booking-bot.json"),
+   // file("${path.module}/../../bots/examples/booking-bot.json"),
     //file("${path.module}/../../bots/examples/minimal-bot.json")
     file("${path.module}/../../bots/examples/pizza-order-bot.json"),
-    file("${path.module}/../../bots/examples/restaurant-bot.json")
+    //file("${path.module}/../../bots/examples/restaurant-bot.json")
     //file("${path.module}/../../bots/examples/simple-greeting-bot.json")
   )
 }
