09f48a716bfa33fc0076560d78baefd31e0e6c41
Debottam Barman Roy <learner.debottam@gmail.com>
2026-02-14 21:43:10 +0000

    refactor: pipeline

--- bot/locals.tf
+++ bot/locals.tf
@@ -1,15 +1,15 @@
 locals {
-  bot_config = jsonencode(
-    file("${path.module}/examples/pizza-order-bot.json")
+  bot_config = jsondecode(
+    file("${path.module}/examples/minimal-bot.json")
   )
   tags ={
     Application = "My-Bot"
     ManagedBy ="Terraform"
     Evironment = var.environment
     environment = "dev"
-aws_region = var.aws_region
-aws_account_name = var.aws_account_name
-aws_account_id = var.aws_account_id
+    aws_region = var.aws_region
+    aws_account_name = var.aws_account_name
+    aws_account_id = var.aws_account_id
   }
   
 }
