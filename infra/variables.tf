b51fa3cc3e9bcfa2790fa9698b30375fbcf80a24
Debottam Barman Roy <learner.debottam@gmail.com>
2026-02-16 15:35:58 +0000

    refactor: pipeline

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,15 +1,15 @@
-variable "environment" {
-  type = string
-}
-
-variable "aws_region" {
-  type = string
-}
-
-# variable "aws_account_name" {
+# variable "environment" {
 #   type = string
 # }
 
-variable "aws_account_id" {
-  type = string
-}
+# variable "aws_region" {
+#   type = string
+# }
+
+# # variable "aws_account_name" {
+# #   type = string
+# # }
+
+# variable "aws_account_id" {
+#   type = string
+# }
