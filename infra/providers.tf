f6f517d3112c34e92496489b39186972f558ad5c
Debottam Barman Roy <learner.debottam@gmail.com>
2026-02-16 16:03:24 +0000

    refactor: pipeline

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
   allowed_account_ids = [var.aws_account_id]
 
-  default_tags {
-    tags = local.tags
-  }
+  # default_tags {
+  #   tags = local.tags
+  # }
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,15 +1,11 @@
-# variable "environment" {
-#   type = string
-# }
+variable "environment" {
+  type = string
+}
 
-# variable "aws_region" {
-#   type = string
-# }
+variable "aws_region" {
+  type = string
+}
 
-# # variable "aws_account_name" {
-# #   type = string
-# # }
-
-# variable "aws_account_id" {
-#   type = string
-# }
+variable "aws_account_id" {
+  type = string
+}
