ca779274622d76d00efc8b0bd2c846c82e268707
Debottam Barman Roy <learner.debottam@gmail.com>
2026-02-16 19:04:46 +0000

    refactor: pipeline

--- bot/locals.tf
+++ bot/locals.tf
@@ -3,8 +3,8 @@ locals {
     file("${path.module}/examples/minimal-bot.json")
   )
   tags ={
-    ManagedBy ="Terraform"
-    Evironment = var.environment
+    MANAGED_BY ="Terraform"
+    ENVIRONMENT = var.environment
     AWS_REGION = var.aws_region
     AWS_ACCOUNT_NAME = var.aws_account_name
     AWS_ACCOUNT_ID = var.aws_account_id
